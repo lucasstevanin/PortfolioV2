5a03ffb3d7fd373b1e4c4105150609fd36a22f1a
Lucas Stevanin <lucasstevanin@gmail.com>
2023-04-20 15:30:30 -0300

    cache test express

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,24 +8,9 @@ import Contato from "@/components/Contato";
 import Menu from "@/components/Menu";
 import RedesSociais from "@/components/RedesSociais";
 import Footer from "@/components/Footer";
-import { saveCache, setCache, loadCache } from "@/utils/cache";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    // Armazena o estado atual da página em cache quando o usuário sai da página
-    window.addEventListener("beforeunload", saveCache);
-    return () => {
-      window.removeEventListener("beforeunload", saveCache);
-    };
-  }, []);
-
-  // Usa o cache para evitar a renderização desnecessária da página
-  const cachedData = loadCache();
-  if (cachedData !== undefined) {
-    setCache("data", cachedData);
-  }
-
   return (
     <>
       <Menu />
